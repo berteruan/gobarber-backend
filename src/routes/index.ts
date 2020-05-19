d22e4e5d3fd02b28c627abda4545949d79315341
Ruan Berté <berte.ruan@gmail.com>
2020-05-19 14:37:11 -0300

    Creating User

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+
+// Routers
+import usersRouter from './users.routes';
 import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
+routes.use('/users', usersRouter);
 routes.use('/appointments', appointmentsRouter);
 
 export default routes;
