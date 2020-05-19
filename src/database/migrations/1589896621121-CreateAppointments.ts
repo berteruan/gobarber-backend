5b4e6ae8f28bf59c85494481a102d7b03a4c4a45
Ruan Berté <berte.ruan@gmail.com>
2020-05-19 13:34:47 -0300

    Creating Appointment in database

--- src/database/migrations/1589896621121-CreateAppointments.ts
+++ src/database/migrations/1589896621121-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1589896621121
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
